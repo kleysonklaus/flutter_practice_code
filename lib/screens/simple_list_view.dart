c7409c799093a697ba6ba472895116ff6a75a0ec
Kleyson Klaus Huanatico Luque <khuanati@bancolombia.com.co>
2022-11-21 22:46:00 -0500

    feat(simple list view): create file simple list view

---
+++ lib/screens/simple_list_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SimpleListview extends StatelessWidget {
+  const SimpleListview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Simple List View"),
+      ),
+    );
+  }
+}
