03d4d84252fdb8701f493a0b73ca623e819293de
Kleyson Klaus Huanatico Luque <khuanati@bancolombia.com.co>
2022-11-21 22:43:51 -0500

    feat(fpc_button): fix fields, now use null value in parameters

--- lib/app.dart
+++ lib/app.dart
@@ -14,6 +14,7 @@ class App extends StatelessWidget {
             children: [
               FpcButtom(
                 label: "primera parte",
+                onTap: () {},
               ),
             ],
           ),
--- lib/widgets/fpc_button.dart
+++ lib/widgets/fpc_button.dart
@@ -3,19 +3,36 @@ import 'package:flutter/material.dart';
 class FpcButtom extends StatelessWidget {
   final String label;
   final void Function()? onTap;
+  final double? textFontSize;
+  final FontWeight? textFontWeight;
+  final Color? backgroundColor;
+  final double? maxWith;
+  final double? mnaxHeight;
   const FpcButtom({
     Key? key,
     required this.label,
     this.onTap,
+    this.textFontSize = 16.0,
+    this.textFontWeight,
+    this.backgroundColor = Colors.purple,
+    this.maxWith = 250,
+    this.mnaxHeight = 50,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: onTap,
       child: Text(
         label,
-        style: TextStyle(),
+        style: TextStyle(
+          fontSize: textFontSize,
+          fontWeight: textFontWeight,
+        ),
+      ),
+      style: ElevatedButton.styleFrom(
+        primary: backgroundColor,
+        minimumSize: Size(maxWith!, mnaxHeight!),
       ),
     );
   }
