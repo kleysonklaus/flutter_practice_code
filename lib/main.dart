2068623d382dafc8be6a96f9b6beb7ae3f267b63
Kleyson Klaus Huanatico Luque <khuanati@bancolombia.com.co>
2022-12-10 23:28:38 -0500

    feat(app): MIGRATE flutter 3.3.8

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Practice Code',
       theme: ThemeData(
+        useMaterial3: true,
         primarySwatch: Colors.blue,
       ),
       home: App(),
