f3797ace637c0e3ce92046c3cb0d27faf14c341d
kleyson klaus <kleyson.zur44@gmail.com>
2022-12-22 23:11:55 -0500

    feat(helper_data_mock): implement data mock

---
+++ lib/screens/simular_api/helper/helper_data_mock.dart
@@ -0,0 +1,36 @@
+class HelperDataMock {
+  List<Map<String, dynamic>> getDataPersonsMock() {
+    return [
+      {
+        "nombre": "pepito mock",
+        "apellido": "torres",
+        "edad": 20,
+      },
+      {
+        "nombre": "maria mock",
+        "apellido": "zela",
+        "edad": 22,
+      },
+      {
+        "nombre": "raul mock",
+        "apellido": "ritch",
+        "edad": 23,
+      },
+      {
+        "nombre": "melissa mock",
+        "apellido": "garcia",
+        "edad": 24,
+      },
+      {
+        "nombre": "rinaldi mock",
+        "apellido": "gomez",
+        "edad": 25,
+      },
+      {
+        "nombre": "esther mock",
+        "apellido": "branch",
+        "edad": 26,
+      },
+    ];
+  }
+}
