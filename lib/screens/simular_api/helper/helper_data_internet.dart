eb427eae27a6db07fa22d0cf09ea682184242ad8
kleyson klaus <kleyson.zur44@gmail.com>
2022-12-22 23:12:15 -0500

    feat(helper_data_internet): implement get data from internet

---
+++ lib/screens/simular_api/helper/helper_data_internet.dart
@@ -0,0 +1,36 @@
+class HelperDataInternet {
+  List<Map<String, dynamic>> getDataPersonsInternet() {
+    return [
+      {
+        "nombre": "pepito INTERNET",
+        "apellido": "torres",
+        "edad": 20,
+      },
+      {
+        "nombre": "maria INTERNET",
+        "apellido": "zela",
+        "edad": 22,
+      },
+      {
+        "nombre": "raul INTERNET",
+        "apellido": "ritch",
+        "edad": 23,
+      },
+      {
+        "nombre": "melissa INTERNET",
+        "apellido": "garcia",
+        "edad": 24,
+      },
+      {
+        "nombre": "rinaldi INTERNET",
+        "apellido": "gomez",
+        "edad": 25,
+      },
+      {
+        "nombre": "esther INTERNET",
+        "apellido": "branch",
+        "edad": 26,
+      },
+    ];
+  }
+}
