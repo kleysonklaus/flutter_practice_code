770130e00bd85c2bb0b0bd574f9704dd39cfc626
kleyson klaus <kleyson.zur44@gmail.com>
2022-12-22 23:48:30 -0500

    feat(simple_provider): implement simple provider to gestion two enviorments mock and testing

--- lib/screens/simular_api/simple_provider/simple_provider.dart
+++ lib/screens/simular_api/simple_provider/simple_provider.dart
@@ -1,7 +1,31 @@
+import 'package:flutter_practice_code/screens/simular_api/driven_adapters/gateway/api_gateway.dart';
+import 'package:flutter_practice_code/screens/simular_api/driven_adapters/repository/person_api.dart';
+import 'package:flutter_practice_code/screens/simular_api/driven_adapters/repository_mock/person_api_mock.dart';
 import 'package:flutter_practice_code/screens/simular_api/enum/enviorment.dart';
 
 class SimpleProvider {
+  late ApiGateway api;
+  late Environment current;
+
   setEnvironment({required Environment environment}) {
-    //
+    current = environment;
+    if (environment == Environment.mock) {
+      api = PersonApiMock();
+    } else {
+      api = PersonApi();
+    }
+  }
+
+  String getLabelEnv() {
+    switch (current) {
+      case Environment.mock:
+        return "Mock";
+      case Environment.testing:
+        return "Testing";
+      case Environment.producction:
+        return "Producction";
+      default:
+        return '';
+    }
   }
 }
