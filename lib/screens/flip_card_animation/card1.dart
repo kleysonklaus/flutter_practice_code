bd3f84f492f96aeb00d17c82d26c5035cde8f0c4
kleyson klaus <kleyson.zur44@gmail.com>
2023-02-05 17:04:46 -0500

    feat(flip_card_animation): create first card of type widget

---
+++ lib/screens/flip_card_animation/card1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Card1 extends StatelessWidget {
+  const Card1({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 200,
+      color: Colors.red,
+      child: Center(
+        child: Text("Texto Card 1"),
+      ),
+    );
+  }
+}
